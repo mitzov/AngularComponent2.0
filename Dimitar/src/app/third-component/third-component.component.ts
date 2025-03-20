ad842fe461b4dbb7a586f0baf92654b1a9bdd81d
Dimitar Mitzov <mitzov.dimitar@iisgalvanimi.edu.it>
2025-03-20 12:59:47 +0000

    punto 1

---
+++ Dimitar/src/app/third-component/third-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FourthComponentComponent } from '../fourth-component/fourth-component.component';
+
+@Component({
+  selector: 'app-third-component',
+  imports: [FourthComponentComponent],
+  templateUrl: './third-component.component.html',
+  styleUrl: './third-component.component.css'
+})
+export class ThirdComponentComponent {
+  name = "hird";
+}
